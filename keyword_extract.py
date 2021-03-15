020de24ccc2aebee0703c86708722f76e13a239d
RichardYCX <feavfas@gmail.com>
2021-03-15 23:38:44 +0800

    Refactored Code

--- keyword_extract.py
+++ keyword_extract.py
@@ -2,33 +2,51 @@ import config
 import json
 import requests
 
-file_name = 'RY.json'
 
-with open(file_name, 'r') as json_file:
-    person_dict = json.load(json_file)
+def read_from_json(file_name: str) -> dict:
+    with open(file_name, 'r') as json_file:
+        return json.load(json_file)
 
-# print(json.dumps(person_dict, indent=4, sort_keys=True))
-text = ""
-for x in person_dict["Interests"].keys():
-    if x == 'Companies':
-        pass
-    elif x == 'Influencers':
-        y = person_dict["Interests"][x]
-        for z in y:
-            text += z["Description"] + ' '
 
-if "Recent Activities" in person_dict:
-    for x in person_dict["Recent Activities"]:
-            y = x["Author Description"]
-            text += (y+' ') if y is not None else ''
+class KeywordExtractor:
+    def __init__(self, person_dict):
+        self.text = ""
+        self.person_dict = person_dict
+        self.keywords = ""
 
-if text != "":
-    r = requests.post(
-        "https://api.deepai.org/api/text-tagging",
-        data={'text': text},
-        headers={'api-key': config.api_key}
-    )
+        self.extract_information()
+        if self.text != "":
+            self.extract_keywords()
 
-    print(r.json()['output'])
-else:
-    print("no keywords")
+    def extract_information(self):
+        for x in self.person_dict["Interests"].keys():
+            if x == 'Companies':
+                pass
+            elif x == 'Influencers':
+                y = self.person_dict["Interests"][x]
+                for z in y:
+                    self.text += z["Description"] + '. '
+
+        if "Recent Activities" in self.person_dict:
+            for x in self.person_dict["Recent Activities"]:
+                    y = x["Author Description"]
+                    self.text += (y+'. ') if y is not None else ''
+
+    def extract_keywords(self):
+        r = requests.post(
+            "https://api.deepai.org/api/text-tagging",
+            data={'text': self.text},
+            headers={'api-key': config.api_key}
+        )
+        self.keywords = r.json()['output']
+
+
+def main():
+    file_name = 'RY.json'
+    person_dict = read_from_json(file_name=file_name)
+    person = KeywordExtractor(person_dict=person_dict)
+    print(person.keywords)
+
+
+if __name__ == "__main__":
+    main()
